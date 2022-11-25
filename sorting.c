f3807193790cce19b14ec02cca0a5a36ba413f2d
Aditya Bhattad <adityabhattad18@gmail.com>
2022-11-25 21:35:02 +0530

    Add Shell Sort

--- sorting.c
+++ sorting.c
@@ -11,7 +11,7 @@
 
     7. Tree Sort -Time Complexity: O(nlog(n)) Comparsion Basd Sorts
 
-    8. Shell Sort -Time Complexity: O(n^(3/2)) Comparsion Basd Sorts
+    8. Shell Sort -Time Complexity: O(n^(3/2)) or O(n log(n)) Comparsion Basd Sorts  ✅
 
 
     9. Count Sort -Time Complexity: O(n) Index Based Sorts
@@ -213,8 +213,8 @@ void mergeSort(int arry[], int size)
     {
         return;
     }
-    int leftArrySize=size/2;
-    int rightArrySize=size-leftArrySize;
+    int leftArrySize = size / 2;
+    int rightArrySize = size - leftArrySize;
     int leftArry[leftArrySize];
     int rightArry[rightArrySize];
     int leftArryIndex = 0, rightArryIndex = 0;
@@ -224,15 +224,17 @@ void mergeSort(int arry[], int size)
         {
             leftArry[leftArryIndex] = arry[i];
             leftArryIndex++;
-        }else{
-            rightArry[rightArryIndex]=arry[i];
+        }
+        else
+        {
+            rightArry[rightArryIndex] = arry[i];
             rightArryIndex++;
         }
     }
-    mergeSort(leftArry,leftArrySize);
-    mergeSort(rightArry,rightArrySize);
+    mergeSort(leftArry, leftArrySize);
+    mergeSort(rightArry, rightArrySize);
     // merge(arry,leftArry,rightArry,size);
-    
+
     int leftArraySize = size / 2;
     int rightArraySize = size - leftArraySize;
     int l = 0, r = 0, main = 0;
@@ -265,6 +267,32 @@ void mergeSort(int arry[], int size)
     }
 }
 
+/*
+    SHELL SORT
+    It is an in-place comparison sort.
+    It is mainly variation of sorting by exchange (bubble sort) or sorting by insertion (insertion sort)
+    It is adaptive
+    It is not stable
+    Time complexity: best: O(n log(n)) worst: O(n^2)
+    Space complexity: O(1)
+*/
+void shellSort(int arry[], int size)
+{
+    for (int gap = size / 2; gap > 0; gap /= 2)
+    {
+        for (int x = gap; x < size; x++)
+        {
+            int temp = arry[x];
+            int y = 0;
+            for (y = x; y >= gap && arry[y - gap] > temp; y -= gap)
+            {
+                arry[y] = arry[y - gap];
+            }
+            arry[y] = temp;
+        }
+    }
+}
+
 #include <stdio.h>
 
 int main()
@@ -276,7 +304,7 @@ int main()
     {
         printf(" %d ", array[x]);
     }
-    mergeSort(array, sizeOfArray);
+    shellSort(array, sizeOfArray);
     printf("\nSorted array is   : ");
     for (int x = 0; x < sizeOfArray; x++)
     {
