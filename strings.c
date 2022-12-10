1cbf16c41fd24922be41abd54551fbc5447a769f
Aditya Bhattad <adityabhattad18@gmail.com>
2022-12-10 16:48:38 +0530

    Add function to reverse a string

--- strings.c
+++ strings.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /*
     1. Character Set/ASCII (American Standard Code for Information Interchange) Codes
@@ -15,61 +15,92 @@
     Space - 13
     escape - 27
 */
-void findLengthOfString(){
+void findLengthOfString()
+{
     char string[100];
     printf("Enter a string: ");
-    fgets(string,100,stdin);
+    fgets(string, 100, stdin);
     fflush(stdin);
-    int length=0;
+    int length = 0;
     int x = 0;
 
-    while(string[x]!='\0'){
+    while (string[x] != '\0')
+    {
         x++;
-        if(string[x]!=' '){
+        if (string[x] != ' ')
+        {
             length++;
         }
     }
     // As it will increment one more time, so that the condition returns false.
     length--;
-    printf("The string you entered was %s and it is %d characters long.",string,length); 
+    printf("The string you entered was %s and it is %d characters long.", string, length);
 }
 
-void changeCaseOfString(){
-    char string[100],toConvert;
+void changeCaseOfString()
+{
+    char string[100], toConvert;
     printf("Enter a string: ");
-    fgets(string,100,stdin);
+    fgets(string, 100, stdin);
     fflush(stdin);
-    for(int x = 0;string[x]!='\0';x++){
-        if(string[x]>='A' && string[x]<='Z'){
-            string[x]+=' ';
+    for (int x = 0; string[x] != '\0'; x++)
+    {
+        if (string[x] >= 'A' && string[x] <= 'Z')
+        {
+            string[x] += ' ';
         }
-        else if(string[x]>='a' && string[x]<='z'){
-            string[x]-=' ';
+        else if (string[x] >= 'a' && string[x] <= 'z')
+        {
+            string[x] -= ' ';
         }
-    } 
-    printf("The string is %s\n",string);
+    }
+    printf("The string is %s\n", string);
 }
 
 // Validting a string: A string will be valid if it only consists of letters,numbers
-int valid(char *string){
+int valid(char *string)
+{
     int x = 0;
-    while(string[x]!='\0'){
-        if(!(string[x]>='A' && string[x]<='Z') && !(string[x]>='a' && string[x]<='z') && !(string[x]>='1' && string[x]<='9')){
+    while (string[x] != '\0')
+    {
+        if (!(string[x] >= 'A' && string[x] <= 'Z') && !(string[x] >= 'a' && string[x] <= 'z') && !(string[x] >= '1' && string[x] <= '9'))
+        {
             return 0;
         }
         x++;
     }
     return 1;
 }
-// Reversing a string 
+// Reversing a string
+void reverseString(char *string)
+{
+    int lengthOfString = 0;
+    while (string[lengthOfString] != '\0')
+    {
+        lengthOfString++;
+    }
+    char buffer[lengthOfString];
+    int bufferIndex = 0;
+    for (int x = lengthOfString - 1; x >= 0; x--)
+    {
+        buffer[bufferIndex] = string[x];
+        bufferIndex++;
+    }
+    int bufferLen=0;
+    while(buffer[bufferLen]!='\0'){
+        string[bufferLen]=buffer[bufferLen];
+        bufferLen++;
+    }
+    
+}
+
 // Checking for palindrome
 // Checking duplicate in a string
 
-int main(){
-    if(valid("Aditya123#4")){
-        printf("String is valid\n");
-    } else{
-        printf("String is not valid\n");
-    }
+int main()
+{
+    char string[] = "Aditya Bhattad";
+    reverseString(string);
+    printf("Reverse of the string is %s\n",string);
     return 0;
 }
