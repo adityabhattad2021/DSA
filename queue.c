fce2415e3b2c79e8d09463b53a9c37b758ef6819
Aditya Bhattad <adityabhattad18@gmail.com>
2022-12-12 17:50:57 +0530

    Add queue theory+code

---
+++ queue.c
@@ -0,0 +1,28 @@
+/*
+    What is queue?
+    => Queue is Linear data sturucture which operates in FIFO(FIrst in First Out) or  LILO(Last in Last Out).
+    - It is named queue as it behaves like a real-world queue, for example - queue(line) of cars in a simgle lane, queue of people waiting at a food counter.
+    - Queue is an abstract data type with a bounded(predefined) capacity.
+    - It is a simple data structure that allows adding and removing elements in a perticular order.
+*/
+
+/*
+    Standard Queue Operations
+    - Enqueue() - Elements are added from one end (rear/back)
+    - Dequeue() - Elements are removed from one end (head/front)
+    - isFull() - Checks if the queue is full. ✅
+    - isEmpty() - Checks if the queue is empty. ✅
+    - Count() - Gets count of total items in queue. ✅
+*/
+
+
+/*
+    Some application of queue data sturctures.
+    - Queue is used when things but have to be processed in First in First Out order.
+    - CPU sheduling, Disk sheduling.
+    - Handling of interrupts in realtime systems. the interupts are handled in the same order as they arrive, first come first served.
+    - In real life, Call Centers phone systems will use queues, to hold people calling them in an order, until a service representative is free.
+    - When data is transferred asynchronously between two processes. Queue is used for synchronization.
+*/
+
+
---
+++ queue.h
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#define SIZE 5
+
+typedef struct Queue
+{
+    int _[SIZE];
+    int frontIndex;
+    int rearIndex;
+} queue;
+
+int initializeQueue(queue *q)
+{
+    q->frontIndex = -1;
+    q->rearIndex = -1;
+    for (int x = 0; x < SIZE; x++)
+    {
+        q->_[x] = 0;
+    }
+}
+
+int isEmpty(queue q)
+{
+    if (q.frontIndex == -1 && q.rearIndex == -1)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isFull(queue q)
+{
+    if (q.rearIndex > SIZE - 2)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int count(queue q)
+{
+    if (q.frontIndex == -1 && q.rearIndex == -1)
+    {
+        return 0;
+    }
+    return (q.rearIndex - q.frontIndex) + 1;
+}
+
+void enqueue(queue *q,int value){
+    if(q->rearIndex > SIZE - 2){
+        printf("\nqueue, overflow.\n");
+        return;
+    }
+    else{
+        if(q->frontIndex == -1 && q->rearIndex == -1){
+            q->frontIndex=0;
+            q->rearIndex=0;
+        }else{
+            q->rearIndex++;
+        }
+        q->_[q->rearIndex]=value;
+    }
+}
+
+int dequeue(queue *q,int value){
+    int elementToReturn=-999;
+    if(q->frontIndex == -1 && q->rearIndex == -1){
+        printf("\nqueue, is already is empty.\n");
+        return elementToReturn;
+    }else{
+        if(q->frontIndex==q->rearIndex){
+            elementToReturn=q->_[q->frontIndex];
+            q->_[q->frontIndex]=0;
+            q->frontIndex=-1;
+            q->rearIndex=-1;
+        }else{
+            elementToReturn=q->_[q->frontIndex];
+            q->_[q->frontIndex]=0;
+            q->frontIndex++;
+        }
+    }
+    return elementToReturn;
+}
