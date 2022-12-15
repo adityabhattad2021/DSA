b2ad4fe616962d2945fe5f254f44922cb1e211bf
Aditya Bhattad <adityabhattad18@gmail.com>
2022-12-15 23:16:49 +0530

    Add element at begainning and end

--- linked_list.c
+++ linked_list.c
@@ -36,7 +36,7 @@
     3. Prepen a new node(to the start) of the list.
     4. Inserting a new node to a specific position in the list.
     5. Deleting a node from the list.
-    Updating a node in the list.
+    6. Updating a node in the list.
 
 
     Some applications of linked lists.
@@ -45,113 +45,134 @@
     3. Implementing hash tables: each bucket of the hash table can itself be a linked list(open chain hashing).
 */
 
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdio.h>
-#include<stdlib.h>
-
-
-typedef struct node{
+typedef struct node
+{
     int data;
     struct node *next;
 } Node;
 
-
-Node *createNewNode(int data){
-    Node *newNode=(Node *)malloc(sizeof(Node));
-    newNode->data=data;
-    newNode->next=NULL;
+Node *createNewNode(int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
     return newNode;
 }
 
-int count_of_nodes(Node *head){
-    int count=0;
-    if(head==NULL){
+int count_of_nodes(Node *head)
+{
+    int count = 0;
+    if (head == NULL)
+    {
         printf("\nThe linked list is empty.\n");
         return 0;
     }
-    Node *ptr=NULL;
-    ptr=head;
-    while(ptr!=NULL){
+    Node *ptr = NULL;
+    ptr = head;
+    while (ptr != NULL)
+    {
         count++;
-        ptr=ptr->next;
+        ptr = ptr->next;
     }
     return count;
 }
 
-void print_the_list(Node *head){
-    if(head==NULL){
+/*Time Complexity: O(n), but can be made O(1) by changing the design of the list i.e. maintaining a pointer to the last element of the list.*/
+void add_at_end(Node *head, Node *newNode)
+{
+    Node *ptr;
+    ptr = head;
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    ptr->next = newNode;
+}
+
+
+/*Time Complexity: O(1).*/
+void add_at_begainning(Node **head, Node *newNode)
+{
+    Node *ptr = NULL;
+    ptr = newNode;
+    newNode->next = *head;
+    *head = ptr;
+}
+
+void print_the_list(Node *head)
+{
+    if (head == NULL)
+    {
         printf("\nThe linked list is empty.\n");
         return;
     }
-    Node *ptr=NULL;
-    ptr=head;
+    Node *ptr = NULL;
+    ptr = head;
     printf("\nThe elements in the linked list are: ");
-    printf("\n(Head,%p)",&(*head));
-    while(ptr!=NULL){
-        printf("\n(%d,  %p)",ptr->data,&(*ptr->next)); 
-        ptr=ptr->next;
+    printf("\n(Head,%p)", &(*head));
+    while (ptr != NULL)
+    {
+        printf("\n(%d,  %p)", ptr->data, &(*ptr->next));
+        ptr = ptr->next;
     }
 }
 
+int main()
+{
 
-int main(){
-
-    Node *head=NULL;
-    head=createNewNode(31);
-       
-    Node *current=NULL;
-    current=createNewNode(25);
-    head->next=current;
-    
-    current=createNewNode(52);
-    head->next->next=current;
-
-    current=createNewNode(48);
-    head->next->next->next=current;
-
-    current=createNewNode(38);
-    head->next->next->next->next=current;
-
-    current=createNewNode(62);
-    head->next->next->next->next->next=current;
-
-    current=createNewNode(84);
-    head->next->next->next->next->next->next=current;
-
+    Node *head = NULL;
+    Node *current = NULL;
+    while (1)
+    {
+        int element;
+        printf("Enter a number to add to the linked list(-999 to quit): ");
+        scanf("%d", &element);
+        if (element == -999)
+        {
+            break;
+        }
+        else
+        {
+            if (head == NULL)
+            {
+                head = createNewNode(element);
+            }
+            else
+            {
+                current = createNewNode(element);
+                add_at_end(head, current);
+            }
+        }
+    }
     print_the_list(head);
-    printf("\nThe number of elements in the current list is %d.\n",count_of_nodes(head));
+    printf("\nThe number of elements in the current list is %d.\n", count_of_nodes(head));
+    while (1)
+    {
+        int element;
+        printf("Enter a number to add at the start of the already generated list linked list(-999 to quit): ");
+        scanf("%d", &element);
+        if (element == -999)
+        {
+            break;
+        }
+        else
+        {
+            if (head == NULL)
+            {
+                head = createNewNode(element);
+            }
+            else
+            {
+                current = createNewNode(element);
+                add_at_begainning(&head, current);
+            }
+        }
+    }
+    print_the_list(head);
+    printf("\nThe number of elements in the current list is %d.\n", count_of_nodes(head));
+    
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
