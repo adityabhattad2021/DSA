0cde01853b42cd4464f6626ae6cef2d7c9e6b574
Aditya Bhattad <adityabhattad18@gmail.com>
2023-01-01 13:30:56 +0530

    Added Infix to Prefix conversion

--- prefix_postfix_infix/code1.c
+++ prefix_postfix_infix/code1.c
@@ -44,6 +44,22 @@
     9. Repeat steps 1 to 8 until the infix expression is completely scanned.
     10. Pop and output from the stack until it is empty.
 */
+
+/*
+    Infix to Prefix conversion using stack.
+    1. Reverse the expression and swap '(' with ')' and  vise-a-versa.
+    2. Put the operands as they arrive
+    3. If the operator arrives and the stack is empty, push to the stack.
+    4. If the incoming operator has higher precedance than the one at the top of the stack, push it onto the stack.
+    5. If the incoming operator has equal precedance with the top of the stack and incoming operator is '^' pop and print the top of the stack continue step 5 till the condition is true.
+    6. If the incoming operator has equal precedance with the top of the stack, push it onto the stack.
+    7. If the incoming operator has lower precedance than the top of the stack, pop and print the top of the stack, continue with step 7 untill condition is true.
+    8. At the end of the expression, pop and print all the operators from the stack.
+    9. If incoming symbol is '(' push it onto the stack.
+    10. If incoming symbol is ')' pop the stack and print all the operators till '(' is found or the stack is empty, at the end pop '(' from the stack.
+    11. If the top of stack is '(', push operator on the stack.
+    12. At the end reverse the output string.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -142,10 +158,42 @@ int precedance(char toCheck)
     }
 }
 
+void reverseTheExpression(char *expression)
+{
+    int length = 0;
+    while (expression[length] != '\0')
+    {
+        length++;
+    }
+    char buffer[length + 1];
+    for (int x = length - 1, y = 0; x >= 0; x--, y++)
+    {
+        if (expression[x] == ')')
+        {
+            buffer[y] = '(';
+        }
+        else if (expression[x] == '(')
+        {
+            buffer[y] = ')';
+        }
+        else
+        {
+            buffer[y] = expression[x];
+        }
+    }
+    buffer[length] = '\0';
+    int a = 0;
+    while (buffer[a] != '\0')
+    {
+        expression[a] = buffer[a];
+        a++;
+    }
+}
+
 void infixToPostfix(char *infix, char *postfix)
 {
     Stack s;
-    initializeStack(30,&s);
+    initializeStack(30, &s);
     int infixIndex = 0, postfixIndex = 0;
     while (infix[infixIndex] != '\0')
     {
@@ -160,34 +208,140 @@ void infixToPostfix(char *infix, char *postfix)
         }
         else if (currentChar == ')')
         {
-            while (!isEmpty(s) || peek(s) != '(')
+            while (!isEmpty(s) && peek(s) != '(')
             {
                 postfix[postfixIndex++] = pop(&s);
             }
-            pop(&s);
-            infixIndex++;
+            if (peek(s) == '(')
+            {
+                pop(&s);
+                infixIndex++;
+            }
         }
         else
         {
-            while (!isEmpty(s)  &&  precedance(peek(s)) >= precedance(currentChar))
+            while (!isEmpty(s) && precedance(peek(s)) >= precedance(currentChar))
             {
                 postfix[postfixIndex++] = pop(&s);
             }
             push(&s, infix[infixIndex++]);
         }
     }
-    while(!isEmpty(s)){
-        postfix[postfixIndex++]=pop(&s);
+    while (!isEmpty(s))
+    {
+        postfix[postfixIndex++] = pop(&s);
     }
-    postfix[postfixIndex]='\0';
+    postfix[postfixIndex] = '\0';
+}
+
+void infixToPrefix(char *infix, char *prefix)
+{
+    Stack s;
+    initializeStack(40, &s);
+    reverseTheExpression(infix);
+    int infixIndex = 0, prefixIndex = 0;
+    while (infix[infixIndex] != '\0')
+    {
+        char currentChar = infix[infixIndex];
+        if (isOperand(currentChar))
+        {
+            prefix[prefixIndex++] = infix[infixIndex++];
+        }
+        else if (currentChar == '(')
+        {
+            push(&s, infix[infixIndex++]);
+        }
+        else if (currentChar == ')')
+        {
+            while (!isEmpty(s) && peek(s) != '(')
+            {
+                prefix[prefixIndex++] = pop(&s);
+            }
+            if (peek(s) == '(')
+            {
+                pop(&s);
+                infixIndex++;
+            }
+        }
+        else
+        {
+            if (isEmpty(s))
+            {
+                push(&s, infix[infixIndex++]);
+            }
+            else if (precedance(peek(s)) < precedance(currentChar))
+            {
+                push(&s, infix[infixIndex++]);
+            }
+            else if (precedance(peek(s)) == precedance(currentChar) && currentChar == '^')
+            {
+                while (!isEmpty(s) && precedance(peek(s)) == precedance(currentChar) && currentChar == '^')
+                {
+                    prefix[prefixIndex++] = pop(&s);
+                }
+                push(&s, infix[infixIndex++]);
+            }
+            else if (precedance(peek(s)) == precedance(currentChar))
+            {
+                push(&s, infix[infixIndex++]);
+            }
+            else
+            {
+                while (!isEmpty(s) && precedance(peek(s)) > precedance(currentChar))
+                {
+                    prefix[prefixIndex++] = pop(&s);
+                }
+                push(&s, infix[infixIndex++]);
+            }
+        }
+    }
+    while (!isEmpty(s))
+    {
+        printf("triggesre\n");
+        prefix[prefixIndex++] = pop(&s);
+    }
+    prefix[prefixIndex] = '\0';
+    printf("\nExpression before reversing is: %s\n", prefix);
+    reverseTheExpression(prefix);
 }
 
 int main()
 {
-    char infix[100], postfix[100];
-    printf("Enter the infix expression: ");
-    scanf("%s", infix);
-    infixToPostfix(infix, postfix);
-    printf("The postfix expression is: %s\n", postfix);
+    char infix[100], postfix[100], prefix[100];
+    int choice,toStop=0;
+    
+    while (!toStop)
+    {
+        printf("\n1. To convert infix to postfix\n");
+        printf("2. To convert infix to prefix\n");
+        printf("3. Stop\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+        {
+            printf("\nEnter the infix expression: ");
+            scanf("%s", infix);
+            infixToPostfix(infix, postfix);
+            printf("The postfix expression is: %s\n", postfix);
+            break;
+        }
+        case 2:
+        {
+            printf("\nEnter the inifx expression: ");
+            scanf("%s", infix);
+            infixToPrefix(infix, prefix);
+            printf("\nThe prefix expression is: %s\n", prefix);
+            break;
+        }
+        case 3:
+        {
+            toStop=1;
+            break;
+        }
+        }
+    }
+
     return 0;
 }
