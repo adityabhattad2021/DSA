9ee5ddb7df42e5d207afc39a157aae1871c93de9
Aditya Bhattad <adityabhattad18@gmail.com>
2023-01-14 15:19:38 +0530

    Add heap structure and basic functions

--- trees3.c
+++ trees3.c
@@ -1,4 +1,4 @@
-/*  
+/*
     What is heap?
     => A heap is a special tree based data structure in which the tree is a complete binary tree.
     (A complete binary tree is a binary tree in which every level, except possibly the last, is completely filled, and all the nodes are as far left as possible.)
@@ -22,7 +22,7 @@
             1. A[(i-1)/2] Return the parent node.
             2. A[(2*i)+1] returns the left child node
             3. A[(2*i)+2]  returns the right child of the node.
-            
+
 
     Operations using Heap Data Structures:
     1. getMini(): it returns the root of the min heap. Time complexity of this operation is O(1).
@@ -33,4 +33,48 @@
     If the new key is greater than its parent, then we don't need to do anything. Otherwise we need to traverse up to fix the voilated heap property.
     5. delete(): Deleting a key also takes O(log(n)) time. We replace the key to deleted with the minimum infinite by calling decreaceKey(). After decreaseKey(), the minus infinite value must reach the root, so we call extractMin() to remove the key.
     6. Heapify(): Heapify is the process of creating a heap datastructure from a binary tree. It is used to create a min-heap or a max-heap.
-*/  
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct min_heap
+{
+    int *arry;
+    int currentSize;
+    int capacity;
+} MinHeap;
+
+void CreateMinHeap(int capacity, MinHeap *h)
+{
+    h->capacity = capacity;
+    h->currentSize = 0;
+    h->arry = (int *)malloc(sizeof(int) * h->capacity);
+}
+
+void linearSearch(MinHeap *h, int toFind)
+{
+    for (int x = 0; x < h->currentSize; x++)
+    {
+        if (h->arry[x] == toFind)
+        {
+            printf("\nElement found at index %d.\n");
+            return;
+        }
+    }
+    printf("\nElement not found.\n");
+}
+
+void print_heap_arry(MinHeap h)
+{
+    printf("Heap array is: ");
+    for (int x = 0; x < h.currentSize; x++)
+    {
+        printf("%d ", h.arry[x]);
+    }
+}
+
+int heap_height(MinHeap h)
+{
+    return ceil(log2(h.currentSize + 1)) - 1;
+}
