c85964db02d77d00b72836a49d6abf081e3d4c9f
Aditya Bhattad <adityabhattad18@gmail.com>
2023-01-19 18:45:25 +0530

    Graph theory 1

---
+++ graphs.c
@@ -0,0 +1,48 @@
+/*
+    Graph data structure
+    => Graph consists of a finite set of vertices (or nodes) and set of edges (or links) which connects a pair of nodes.
+    A graph is a non linear data structure consisting of nodes and edges.
+
+
+    Difference between trees and graphs:
+    Trees:
+    1. Only one path or edge from one node.
+    2. Has a root node.
+    3. Have n-1 edges (N=Number of nodes).
+    4. Hierarchical Model.
+
+    Graphs:
+    1. Multiple path/edges from one node.
+    2. No root node.
+    3. Number of edges not defined.
+    4. Network Modal.
+
+    (Tree is an undirected graph.)
+
+    Definition:
+     - Graph consists of a finite set of vertices (or nodes) and set of edges(or links) which connects the pair of nodes.
+     - Graph is a non-linear data structure consisting of nodes and edges.
+
+    Directed and Undirected Graphs:
+
+    Directed Graphs (Digraphs):
+    A directed graph is a set of vertices (nodes) connected by edges, with each node having a direction associated with it.
+    Edges are usually represented by arrow pointing in the direction the graph can be traversed.
+
+    Undirected Graphs:
+    In an undirected graphs the edges are bidirectional, wit no direction associatd with them. Hence, the graph can be traversed in either direction. The absence of an arrow tells us that the graph is undirected.
+
+    Weighted and Unweighted Graphs:
+
+    Weighted Graphs:
+    A weighted graph is a graph in which each branch is given a numerical weight.
+
+    Unweighted Graphs:
+    An unweighted graph is a group in which all the edges/paths are considered to have same weight.
+
+    Real World use cases of graphs:
+    - Social Network
+    - Maps (Google Maps) / GPS / Nvigation / Flights
+    - World Wide Web
+
+*/
