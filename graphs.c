7d7033d026b81c82f869fb77af23d5109c3ec861
Aditya Bhattad <adityabhattad18@gmail.com>
2023-01-22 12:03:58 +0530

    Add Linked List for Graph Traversal

--- graphs.c
+++ graphs.c
@@ -46,3 +46,135 @@
     - World Wide Web
 
 */
+
+/*
+    Adjancency Matrix:
+    An adjancency matrix is a way of representing a graph as a matrix of boolean (Os and 1s). A finite graph can be represented in the form of a square matrix, where the boolean value of the matrix indicates if there is a connection between the nodes.
+*/
+
+// We will have to use queue and stack to perform Breadth first search and Depth first search on the adjacency matrix of the graph.
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} Node;
+
+Node *create_new_node(int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+
+    newNode->data = data;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+void insert_from_beginning(Node **head, int data)
+{
+    Node *newNode = create_new_node(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+        return;
+    }
+    Node *temp = NULL;
+    temp = *head;
+    (*head) = newNode;
+    newNode->next = temp;
+}
+
+void remove_from_beginning(Node **head)
+{
+    if (*head == NULL)
+    {
+        printf("\nThe Linked List is empty.\n");
+        exit(1);
+    }
+    if ((*head)->next == NULL)
+    {
+        free(*head);
+        *head = NULL;
+        return;
+    }
+    Node *temp = NULL;
+    temp = *head;
+    (*head) = (*head)->next;
+    free(temp);
+    temp = NULL;
+}
+
+void insert_from_end(Node **head, int data)
+{
+    Node *newNode = create_new_node(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+        return;
+    }
+    Node *temp = NULL;
+    temp = *head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void remove_from_end(Node **head)
+{
+    if (*head == NULL)
+    {
+        printf("\nLinked List is empty.\n");
+        exit(1);
+    }
+    if ((*head)->next == NULL)
+    {
+        free(*head);
+        *head = NULL;
+        return;
+    }
+    Node *temp = NULL, *temp2 = NULL;
+    temp = *head;
+    while (temp->next != NULL)
+    {
+        temp2 = temp;
+        temp = temp->next;
+    }
+    temp2->next = NULL;
+    free(temp);
+    temp = NULL;
+}
+
+void list_traversal(Node *head)
+{
+    if (head == NULL)
+    {
+        printf("\nLinked List is empty.\n");
+        return;
+    }
+    printf("\nElements of the linked list are: ");
+    while (head)
+    {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main()
+{
+    Node *head;
+    head = NULL;
+    insert_from_beginning(&head, 21);
+    insert_from_beginning(&head, 32);
+    insert_from_beginning(&head, 43);
+    insert_from_beginning(&head, 25);
+    remove_from_beginning(&head);
+    insert_from_end(&head, 48);
+    insert_from_end(&head, 85);
+    insert_from_end(&head, 64);
+    remove_from_end(&head);
+    list_traversal(head);
+}
