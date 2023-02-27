503eba86437b6f5024a02485344bb6964fbd7ad0
Aditya Bhattad <adityabhattad18@gmail.com>
2023-02-27 14:07:22 +0530

    BFT Clearer and with recursion

--- graphs.c
+++ graphs.c
@@ -62,112 +62,20 @@ typedef struct node
     struct node *next;
 } Node;
 
-Node *create_new_node(int data)
-{
-    Node *newNode = (Node *)malloc(sizeof(Node));
-
-    newNode->data = data;
-    newNode->next = NULL;
-
-    return newNode;
-}
-
-void insert_from_beginning(Node **head, int data)
-{
-    Node *newNode = create_new_node(data);
-    if (*head == NULL)
-    {
-        *head = newNode;
-        return;
-    }
-    Node *temp = NULL;
-    temp = *head;
-    (*head) = newNode;
-    newNode->next = temp;
-}
-
-void remove_from_beginning(Node **head)
-{
-    if (*head == NULL)
-    {
-        printf("\nThe Linked List is empty.\n");
-        exit(1);
-    }
-    if ((*head)->next == NULL)
-    {
-        free(*head);
-        *head = NULL;
-        return;
-    }
-    Node *temp = NULL;
-    temp = *head;
-    (*head) = (*head)->next;
-    free(temp);
-    temp = NULL;
-}
-
-void insert_from_end(Node **head, int data)
-{
-    Node *newNode = create_new_node(data);
-    if (*head == NULL)
-    {
-        *head = newNode;
-        return;
-    }
-    Node *temp = NULL;
-    temp = *head;
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    temp->next = newNode;
-}
-
-void remove_from_end(Node **head)
-{
-    if (*head == NULL)
-    {
-        printf("\nLinked List is empty.\n");
-        exit(1);
-    }
-    if ((*head)->next == NULL)
-    {
-        free(*head);
-        *head = NULL;
-        return;
-    }
-    Node *temp = NULL, *temp2 = NULL;
-    temp = *head;
-    while (temp->next != NULL)
-    {
-        temp2 = temp;
-        temp = temp->next;
-    }
-    temp2->next = NULL;
-    free(temp);
-    temp = NULL;
-}
-
-void list_traversal(Node *head)
-{
-    if (head == NULL)
-    {
-        printf("\nLinked List is empty.\n");
-        return;
-    }
-    printf("\nElements of the linked list are: ");
-    while (head)
-    {
-        printf("%d ", head->data);
-        head = head->next;
-    }
-}
-
 typedef struct queue
 {
     Node *head;
     Node *tail;
 } Queue;
+
+Node *create_new_node(int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    return newNode;
+}
 
 void initializeQueue(Queue *q)
 {
@@ -187,7 +95,7 @@ int isQueueEmpty(Queue q)
 void enqueue(Queue *q, int data)
 {
     Node *newNode = create_new_node(data);
-    if (q->head == NULL && q->tail == NULL)
+    if (isQueueEmpty(*q))
     {
         q->head = newNode;
         q->tail = newNode;
@@ -196,12 +104,14 @@ void enqueue(Queue *q, int data)
     {
         if (q->head == q->tail)
         {
+            q->head->next = newNode;
             q->tail = newNode;
-            q->head->next = q->tail;
-            return;
         }
-        q->tail->next = newNode;
-        q->tail = q->tail->next;
+        else
+        {
+            q->tail->next = newNode;
+            q->tail = newNode;
+        }
     }
 }
 
@@ -210,66 +120,48 @@ int dequeue(Queue *q)
     int toReturn;
     if (isQueueEmpty(*q))
     {
-        printf("\nQueue is already empty.\n");
+        printf("Queue is already empty.\n");
         exit(1);
-    }
-    if (q->head == q->tail)
-    {
-        toReturn = q->head->data;
-        free(q->head);
-        q->head = NULL;
-        q->tail = NULL;
     }
     else
     {
-        Node *temp = NULL;
-        temp = q->head;
-        q->head = q->head->next;
-        toReturn = temp->data;
-        free(temp);
-        temp = NULL;
+        if (q->head == q->tail)
+        {
+            toReturn = q->head->data;
+            free(q->tail);
+            q->tail = NULL;
+            q->head = NULL;
+        }
+        else
+        {
+            Node *temp = NULL;
+            temp = q->head;
+            q->head = q->head->next;
+            toReturn = temp->data;
+            free(temp);
+            temp = NULL;
+        }
     }
     return toReturn;
 }
 
-// Breadth first traversal
-void BFT(int matrix[][7], int start, int number_of_ele)
+int peek(Queue q)
 {
-    Queue q;
-    initializeQueue(&q);
-    int visited[7] = {0, 0, 0, 0, 0, 0, 0};
-    int i;
-    printf("%d ", start);
-    enqueue(&q, start);
-    visited[start] = 1;
-    while (!isQueueEmpty(q))
-    {
-        i = dequeue(&q);
-        for (int j = 1; j < number_of_ele; j++)
-        {
-            if (matrix[i][j] == 1 && visited[j] == 0)
-            {
-                printf("%d ", j);
-                enqueue(&q, j);
-                visited[j] = 1;
-            }
-        }
-    }
+    return q.head->data;
 }
 
-// Depth First Trversal
-void DFT(int matrix[][7], int start, int num_of_ele)
+// Breadth first traversal.
+int BFT(Queue *q,int matrix[][7],int size,int start)
 {
-    static int visited[7] = {0};
-    if (visited[start] == 0)
-    {
-        printf("%d ", start);
-        visited[start] = 1;
-        for (int j = 1; j < num_of_ele; j++)
-        {
-            if (matrix[start][j] == 1 && visited[j] == 0)
-            {
-                DFT(matrix,j,num_of_ele);
+    static int visited[7]={0};
+    printf("%d ",start);
+    visited[start]=1;
+    enqueue(q,start);
+    while(!isQueueEmpty(*q)){
+        int i=dequeue(q);
+        for(int x=1;x<=size;x++){
+            if(matrix[start][x]==1 && visited[x]==0){
+                BFT(q,matrix,size,x);
             }
         }
     }
@@ -284,10 +176,12 @@ int main()
                                   {0, 0, 1, 1, 0, 1, 1},
                                   {0, 0, 0, 0, 1, 0, 0},
                                   {0, 0, 0, 0, 1, 0, 0}};
+    Queue q;
+    initializeQueue(&q);
 
-    // Breadth first traversal
-    // BFT(adjencency_matrix, 1, 7);
+    printf("Breadth first traversal of the graph is: ");
+    BFT(&q,adjencency_matrix,7,6);
 
-    // Depth First Trversal
-    DFT(adjencency_matrix, 2, 7);
+
+    return 0;
 }
